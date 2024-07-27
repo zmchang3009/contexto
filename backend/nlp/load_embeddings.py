5b858ba709c0c33b46e1a170aed345c9e04de430
zmchang <zheming2002@gmail.com>
2024-07-27 22:36:24 +0800

    added basic word checks and db doc remarks

--- backend/nlp/load_embeddings.py
+++ backend/nlp/load_embeddings.py
@@ -1,22 +1,32 @@
 ## Load embeddings from txt to a dict
-## TODO: Add checks to filter for only relevant words
 import numpy as np
 
-def load_embeddings():
+def load_embeddings(count=10):
     print('Loading GloVe model...')
     glove_vocab = {}
 
     try:
-        with open('backend/nlp/glove_6B_50d.txt', 'r') as f:
+        with open('backend/nlp/glove_6B_50d.txt', 'r', encoding='utf-8') as f:
             for line in f:
                 split_line = line.split()
                 ## Get word and embedding (as list of floats)
                 word = split_line[0]
                 embedding = np.array(split_line[1:], dtype=np.float64).tolist()
                 glove_vocab[word] = embedding 
+                
+                count -= 1
+                if count == 0:
+                    break
     except FileNotFoundError:
         print('Error: GloVe embeddings txt file not found')
+    except:
+        print('Error loading embeddings')
     else:
         print(f'{len(glove_vocab)} words loaded')
 
     return glove_vocab
+
+
+if __name__ == '__main__':
+    embeddings = load_embeddings()
+    print(list(embeddings.keys()))
--- backend/nlp/post_embeddings.py
+++ backend/nlp/post_embeddings.py
@@ -4,47 +4,60 @@ import os
 from dotenv import load_dotenv
 from pymongo.mongo_client import MongoClient
 from load_embeddings import load_embeddings
+from word_checks import *
 
 load_dotenv()
 MONGO_URI = os.getenv('MONGO_URI')
 
-## Establish connection to MongoDB
-## Create a new client and connect to the server
-client = MongoClient(MONGO_URI)
 
-## Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print('Pinged your deployment. You successfully connected to MongoDB!')
-except Exception as e:
-    print('Error connecting to MongoDB: ', e)
+def post_embeddings():
+    ## Establish connection to MongoDB
+    ## Create a new client and connect to the server
+    client = MongoClient(MONGO_URI)
 
-
-## Establish connection to 'embeddings' collection
-try:
-    db = client.get_database('test')
-    collection = db.get_collection('embeddings')
-    print('Successfully accessed "embeddings" collection')
-except Exception as e:
-    print('Error connecting to "embeddings" collection: ', e)
+    ## Send a ping to confirm a successful connection
+    try:
+        client.admin.command('ping')
+        print('Pinged your deployment. You successfully connected to MongoDB!')
+    except Exception as e:
+        print('Error connecting to MongoDB: ', e)
 
 
-## Add embeddings to database
-embeddings_dict = load_embeddings()
+    ## Establish connection to 'embeddings' collection
+    try:
+        db = client.get_database('test')
+        collection = db.get_collection('embeddings')
+        print('Successfully accessed "embeddings" collection')
+    except Exception as e:
+        print('Error connecting to "embeddings" collection: ', e)
 
-try:
-    print(f'Adding {len(embeddings_dict)} embeddings...')
-    for word, embedding in embeddings_dict.items():
-        doc = {
-            'word': word,
-            'embedding': embedding
-        }
-        _ = collection.insert_one(doc)
-except Exception as e:
-    print('Error adding embeddings to database: ', e)
-else:
-    print('Embeddings successfully added! \nRetrieving data...')
-    ## Printing the data inserted 
-    cursor = collection.find() 
-    for doc in cursor: 
-        print(doc) 
+
+    ## Add embeddings to database
+    embeddings_dict = load_embeddings(10)
+
+    try:
+        print(f'Adding {len(embeddings_dict)} embeddings...')
+        for word, embedding in embeddings_dict.items():
+            ## Check words before adding to database
+            ## TODO: Add more checks / remark types if necessary
+            if not match_valid_regex(word):
+                continue
+
+            doc = {
+                'word': word,
+                'embedding': embedding,
+                'remark': 'stopword' if is_stopword(word) else '' 
+            } 
+            _ = collection.insert_one(doc)
+    except Exception as e:
+        print('Error adding embeddings to database: ', e)
+    else:
+        print('Embeddings successfully added! \nRetrieving data...')
+        ## Printing the data inserted 
+        cursor = collection.find() 
+        for doc in cursor: 
+            print(doc) 
+
+
+if __name__ == '__main__':
+    post_embeddings()
