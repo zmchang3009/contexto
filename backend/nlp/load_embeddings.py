89e1fa5f86ab69d98381d7262bbda56ab544ed8e
zmchang <zheming2002@gmail.com>
2024-07-28 18:07:53 +0800

    added option to load embeddings from txt or db

--- backend/nlp/load_embeddings.py
+++ backend/nlp/load_embeddings.py
@@ -1,9 +1,55 @@
 ## Load embeddings from txt to a dict
 import numpy as np
+import os
+from dotenv import load_dotenv
+from pymongo.mongo_client import MongoClient
+from word_checks import *
 
-def load_embeddings(count=10):
+load_dotenv()
+MONGO_URI = os.getenv('MONGO_URI')
+
+## Method to load embeddings from database, filtering based on remarks
+def load_embeddings_from_db(db, collection, count=None, exclude=[]):
+    print('Retrieving embeddings from database...')
+    db_embeddings = {}
+    client = MongoClient(MONGO_URI)
+    counter = count if count else 1
+
+    try:
+        ## Send a ping to confirm a successful connection
+        client.admin.command('ping')
+        print('Successfully connected to MongoDB!')
+
+        ## Get embeddings collection as dict
+        db = client.get_database(db)
+        collection = db.get_collection(collection)
+
+        for doc in collection.find():
+            ## Exclude words if necessary
+            if 'stopwords' in exclude and is_stopword(doc['word']):
+                continue
+            if 'invalid_regex' in exclude and not match_valid_regex(doc['word']):
+                continue
+
+            db_embeddings[doc['word']] = doc['embedding']
+            
+            ## If count is specified, check break condition
+            if count:
+                counter -= 1
+            if counter == 0:
+                break
+        print('Successfully loaded embeddings from database')
+    except Exception as e:
+        print('Error loading embeddings from database: ', e)
+
+    return db_embeddings
+
+
+## Method to load embeddings from offline GloVe txt
+def load_embeddings_from_txt(count=None, exclude=[]):
     print('Loading GloVe model...')
     glove_vocab = {}
+    counter = count if count else -1
 
     try:
         with open('backend/nlp/glove_6B_50d.txt', 'r', encoding='utf-8') as f:
@@ -12,10 +58,19 @@ def load_embeddings(count=10):
                 ## Get word and embedding (as list of floats)
                 word = split_line[0]
                 embedding = np.array(split_line[1:], dtype=np.float64).tolist()
+
+                ## Exclude words if necessary
+                if 'stopwords' in exclude and is_stopword(word):
+                    continue
+                if 'invalid_regex' in exclude and not match_valid_regex(word):
+                    continue
+
                 glove_vocab[word] = embedding 
                 
-                count -= 1
-                if count == 0:
+                ## If count is specified, check break condition
+                if count:
+                    counter -= 1
+                if counter == 0:
                     break
     except FileNotFoundError:
         print('Error: GloVe embeddings txt file not found')
@@ -27,6 +82,7 @@ def load_embeddings(count=10):
     return glove_vocab
 
 
+
 if __name__ == '__main__':
-    embeddings = load_embeddings()
+    embeddings = load_embeddings_from_txt(count=10, exclude=['invalid_regex'])
     print(list(embeddings.keys()))
